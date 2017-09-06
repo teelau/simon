2e73ca1fbd095e280ae839a0b04a24b38a785e14
--add <tommylau94@gmail.com>
2017-09-05 22:15:56 -0700

    add easy task

--- js/simon.js
+++ js/simon.js
@@ -1,12 +1,16 @@
 var KEYS = ['c', 'd', 'e', 'f'];
-var NOTE_DURATION = 1000;
+//var start_key = ['s'];
+//var simon_game_h = false
 
+var NOTE_DURATION = 300;
+var DELAY = 2500;
 // NoteBox
 //
 // Acts as an interface to the coloured note boxes on the page, exposing methods
 // for playing audio, handling clicks,and enabling/disabling the note box.
 function NoteBox(key, onClick) {
 	// Create references to box element and audio element.
+
 	var boxEl = document.getElementById(key);
 	var audioEl = document.getElementById(key + '-audio');
 	if (!boxEl) throw new Error('No NoteBox element with id' + key);
@@ -51,10 +55,32 @@ function NoteBox(key, onClick) {
 
 	// Call this NoteBox's clickHandler and play the note.
 	this.clickHandler = function () {
+		
+
+
 		if (!enabled) return;
 
 		this.onClick(this.key)
 		this.play()
+
+		//push things to an array
+		//if(!simon_game_h){
+			queue.push(key);
+			console.log(queue);
+
+			// https://stackoverflow.com/questions/1472705/resetting-a-settimeout
+			clearTimeout(timeout_ref)
+			console.log(timeout_ref)
+			timeout_ref = setTimeout(function () {
+				queue.forEach(function(key, i) {
+					setTimeout(notes[key].play.bind(null, key), i * DELAY);
+				});
+				
+				//destroy array
+				queue.length = 0;
+				console.log("delete")
+			}, 2500);
+		//}
 	}.bind(this)
 
 	boxEl.addEventListener('mousedown', this.clickHandler);
@@ -66,11 +92,17 @@ function NoteBox(key, onClick) {
 // clicking the corresponding boxes on the page will play the NoteBox's audio.
 // It will also demonstrate programmatically playing notes by calling play directly.
 var notes = {};
+//array keeps track of keys
+var queue = [];
+var timeout_ref;
 
 KEYS.forEach(function (key) {
 	notes[key] = new NoteBox(key);
 });
 
 KEYS.concat(KEYS.slice().reverse()).forEach(function(key, i) {
-	setTimeout(notes[key].play.bind(null, key), i * NOTE_DURATION);
+	setTimeout(notes[key].play.bind(null, key), i * NOTE_DURATION/4);
 });
+
+//var queue_simon = [];
+
